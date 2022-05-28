c6470a0bf55586992e67dcc8bae08dc8fdc85c4f
Sai Bhanu <saibhanu301@gmail.com>
2022-05-28 20:51:23 +0530

    portfolio deployed

--- src/components/Header/content.js
+++ src/components/Header/content.js
@@ -7,41 +7,43 @@ export default function Headercontent() {
     <>
       <section className="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
         <div className="container intro">
-          <div className="d-sm-flex align-items-center justify-content-between">
-            <div>
-              <h1 className="display-5 fw-bold">
-                Hi, I'm <span className="text-warning">Sai Bhanu</span>
-              </h1>
-              <h2 className="fs-3 lh-1 mb-3">I love to build amazing apps</h2>
-              <p className="lead my-4">
-                I'm very ambitious self taught developer looking for a role in
-                frontend development in a growing IT organisation with the
-                opportunity to work with the latest technologies on challenging
-                and diverse projects
-              </p>
-              <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                <a
-                  href="#contact"
-                  className="btn btn-primary btn-lg px-4 me-md-2"
-                  role="button"
-                >
-                  Contact Me
-                </a>
-                <a
-                  href={DownloadResume}
-                  className="btn btn-outline-secondary btn-lg px-4 me-md-2"
-                  download
-                >
-                  Download Resume
-                </a>
+          <div className="row">
+            <div className="d-sm-flex align-items-center justify-content-between">
+              <div>
+                <h1 className="display-5 fw-bold">
+                  Hi, I'm <span className="text-warning">Sai Bhanu</span>
+                </h1>
+                <h2 className="fs-3 lh-1 mb-3">I love to build amazing apps</h2>
+                <p className="lead my-4">
+                  I'm very ambitious self taught developer looking for a role in
+                  frontend development in a growing IT organisation with the
+                  opportunity to work with the latest technologies on
+                  challenging and diverse projects
+                </p>
+                <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <a
+                    href="#contact"
+                    className="btn btn-primary btn-lg px-4 me-md-2"
+                    role="button"
+                  >
+                    Contact Me
+                  </a>
+                  <a
+                    href={DownloadResume}
+                    className="btn btn-outline-secondary btn-lg px-4 me-md-2"
+                    download
+                  >
+                    Download Resume
+                  </a>
+                </div>
               </div>
+              <img
+                src={HeroImg}
+                className="img-fluid w-50 d-none d-sm-block"
+                loading="lazy"
+                alt="HeaderImage"
+              />
             </div>
-            <img
-              src={HeroImg}
-              className="img-fluid w-50 d-none d-sm-block"
-              loading="lazy"
-              alt="HeaderImage"
-            />
           </div>
         </div>
       </section>
