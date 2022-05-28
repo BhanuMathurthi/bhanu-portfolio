37ba8395baa5cf1e54226e7b7d3bfc20592c46f2
Sai Bhanu <saibhanu301@gmail.com>
2022-05-28 21:52:26 +0530

    portfolio deployed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Headercontent />
-      <Education />
+      {/* <Education /> */}
       <Skills />
       <Certification />
       <Projects />
