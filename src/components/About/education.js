815eae3026fb7d54b11471f94d88f659134f43c3
Miscellaneous Tasks <bhanumathurthi01@gmail.com>
2022-08-07 21:52:25 +0530

    changes modified

--- src/components/About/education.js
+++ src/components/About/education.js
@@ -4,11 +4,11 @@ export default function Education() {
   return (
     <>
       <section>
-        <div className="container my-4">
-            <h2 className="fs-2 mb-4 text-primary">
-              <strong>Education</strong>
-            </h2>
-          <div className="table-responsive">
+        <div className="container">
+          <h2 className="fs-2 mt-4 mb-3 text-primary">
+            <strong>Education</strong>
+          </h2>
+          <div className="table-responsive d-flex justify-content-center my-4">
             <table className="table">
               <thead className="bg-dark text-white">
                 <tr>
@@ -29,31 +29,21 @@ export default function Education() {
               <tbody>
                 <tr>
                   <th scope="row">2016-2019</th>
-                  <td>
-                    Bachelor of Technology, JNTUK
-                  </td>
-                  <td>
-                    Dadi institute of engineering and technology
-                  </td>
-                  <td>68.03</td>
+                  <td>Bachelor of Technology, JNTUK</td>
+                  <td>Dadi institute of engineering and technology</td>
+                  <td className="text-center">68.03</td>
                 </tr>
                 <tr>
                   <th scope="row">2013-2016</th>
                   <td>Diploma, APSBTET</td>
-                  <td>
-                    Dadi institute of engineering and technology
-                  </td>
-                  <td>83.34</td>
+                  <td>Dadi institute of engineering and technology</td>
+                  <td className="text-center">83.34</td>
                 </tr>
                 <tr>
                   <th scope="row">2012-2013</th>
-                  <td>
-                    SSC, Board of Secondary Education
-                  </td>
-                  <td>
-                    Zilla parishad high school
-                  </td>
-                  <td>7.5</td>
+                  <td>SSC, Board of Secondary Education</td>
+                  <td>Zilla parishad high school</td>
+                  <td className="text-center">7.5</td>
                 </tr>
               </tbody>
             </table>
